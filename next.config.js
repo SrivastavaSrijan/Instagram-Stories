a2535d31f8933bbcb9746bb3cacdff332eefed9f
SrivastavaSrijan <srijan.srivastava35@gmail.com>
2024-08-06 17:14:13 +0530

    fix: json output

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 /** @type {import('next').NextConfig} */
+const dataFilePath = path.join(process.cwd(), 'public', 'json', 'data.json');
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputFileTracingIncludes: {
-      '/api/home': [path.join(__dirname, 'public/json/data.json')],
+      '/api/home': [dataFilePath],
     },
   },
 };
